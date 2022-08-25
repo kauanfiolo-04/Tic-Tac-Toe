c1e44695b664097aabe060e64a1b45498a42b3f6
kauanfiolo-04 <kauanfiolo@hotmail.com>
2022-08-24 23:56:01 -0300

    Adiçoes no JS

--- Tic_Tac_Toe/Scripts/script.js
+++ Tic_Tac_Toe/Scripts/script.js
@@ -1,10 +1,106 @@
 const selection = document.querySelector('.selection');
 const playBoard = document.querySelector('.play-board');
 
+let gameBoard, user='X', computer='O';
+const cells=document.querySelectorAll('.cell');
+const winCombos=[[0,1,2], [3,4,5], [6,7,8], [0,4,8], [6,4,2], [2,5,8], [1,4,7], [0,3,6]];
+
 const playerSelect= player =>{
+
+    user=player;
+    computer=(player==='X')?'O':'X';
+
+    gameBoard=Array.from(Array(9).keys());
+
+    for(let cell of cells){
+        cell.addEventListener('click', handleClick, false);
+    }
+
+    if(computer==='X'){
+        turn(bestSpot(),computer);
+    }
+
     selection.classList.add('fadeOut');
     setTimeout(()=>{selection.style.display='none'},290);
 
     playBoard.classList.add('fadeIn');
     setTimeout(()=>{playBoard.style.display='block'},290);
+}
+
+const startGame=()=>{
+    playBoard.classList.remove('fadeIn');
+    playBoard.classList.add('fadeOut');
+    setTimeout(()=>{playBoard.style.display='none'},290);
+
+    selection.classList.add('fadeIn');
+    setTimeout(()=>{selection.style.display='block'},290);
+
+    for(let cell of cells){
+        cell.innerHTML='';
+        cell.style.color='000';
+        cell.style.background='#CED881';
+    }
+}
+
+startGame();
+
+const handleClick=gameSpace=>{
+    if(typeof gameBoard[gameSpace.target.id]==='number'){
+        turn(gameSpace.target.id, user);
+        if(!checkWin(gameBoard, user) && !checkTie()){
+            setTimeout(()=>{ turn(bestSpot(),computer) },500);
+        }
+    }
+}
+
+const turn=(spaceId, player)=>{
+
+    gameBoard[spaceId]=player;
+    document.getElementById(spaceId).innerHTML=player;
+
+    let gameWon= checkWin(gameBoard, player);
+    if(gameWon){
+        gameOver(gameWon);
+    }
+
+    checkTie();
+}
+
+const checkWin=(board, player)=>{
+    let spaces=board.reduce((acc,ele,idx)=>(ele === player)? acc.concat(idx): acc, []);
+    let gameWon=null;
+
+    for(let[index, winComboSpaces] of winCombos.entries()){
+        if(winComboSpaces.every(elem=> spaces.indexOf(elem)>-1)){
+            gameWon= {index:index, player:player};
+            break;
+        }
+    }
+    return gameWon;
+}
+
+const gameOver=gameWon=>{
+    for(let index of winCombos[gameWon.index]){
+        document.getElementById(index).style.color='#FFF';
+        document.getElementById(index).style.backgrouondColor='#D4FC9D';
+    }
+
+    for(let cell of cells){
+        cell.removeEventListener('click',handleClick, false);
+    }
+
+    declareWinner(gameWon.player===user ? "You won the Game!" : "Computer won the Game!");
+
+}
+
+const checkTie=()=>{
+
+}
+
+const bestSpot=()=>{
+
+}
+
+const declareWinner=()=>{
+    
 }
